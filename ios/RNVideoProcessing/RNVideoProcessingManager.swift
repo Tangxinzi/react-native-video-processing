79bab06eacfc3f36166aa060faa47f968282e6be
KMT 184.05 <oncwnuDpNzQMO7Ay9Yg5TJoyK7Uk@git.weixin.qq.com>
2020-04-14 13:52:47 +0800

    Initial commit

---
+++ ios/RNVideoProcessing/RNVideoProcessingManager.swift
@@ -0,0 +1,29 @@
+//
+//  RNVideoProcessingManager.swift
+//  RNVideoProcessing
+//
+//  Created by Shahen Hovhannisyan on 11/14/16.
+//
+
+import Foundation
+
+@objc(RNVideoProcessingManager)
+class RNVideoProcessingManager: RCTViewManager {
+
+   @objc override func view() -> UIView! {
+       return RNVideoPlayer()
+   }
+
+    @objc override func constantsToExport() -> [AnyHashable: Any] {
+        return [
+          "ScaleNone": AVLayerVideoGravity.resizeAspect,
+          "ScaleToFill": AVLayerVideoGravity.resize,
+          "ScaleAspectFit": AVLayerVideoGravity.resizeAspect,
+          "ScaleAspectFill": AVLayerVideoGravity.resizeAspectFill
+        ]
+    }
+
+    override class func requiresMainQueueSetup() -> Bool {
+        return true
+    }
+}
